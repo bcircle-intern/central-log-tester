3b4873ac91f4064f0a8f9f7674fcd72c141755d8
wk-j <somnuk.wk@gmail.com>
2018-09-26 12:41:37 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Central Log Tester
+
+## Installation
+
+```bash
+dotnet install -g BCircle.CentralLogTester
+```
+
+## Usage
+
+```bash
+bcircle-central-log-tester https://centrallogger-uvbufnedtn.now.sh/#/
+```
---
+++ src/CentralLogTester/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CentralLogProvider;
+
+namespace CentralLogTester {
+
+    class MyService {
+        readonly ILogger<MyService> logger;
+        public MyService(ILogger<MyService> logger) {
+            this.logger = logger;
+        }
+
+        public void Start() {
+            logger.LogInformation("Initialize test app");
+            logger.LogError("This is error");
+            logger.LogTrace("This is critical");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var url = args[0];
+
+            var collection = new ServiceCollection();
+            collection.AddLogging(builder => {
+                builder.AddConsole();
+                builder.AddCentralLog(new CentralLogOptions(url));
+            });
+
+            collection.AddSingleton<MyService>();
+            var provider = collection.BuildServiceProvider();
+            var service = provider.GetService<MyService>();
+            service.Start();
+
+            Console.Read();
+        }
+    }
+}
